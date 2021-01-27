28957f7819bc1acedae835744a438275535aa6f2
Duban Andres <43072051+SrDu001@users.noreply.github.com>
2021-01-27 14:24:16 -0500

    Update factorial.ex
    Optimized factorial

--- 03-loops/factorial.ex
+++ 03-loops/factorial.ex
@@ -15,8 +15,50 @@ defmodule Factorial do
   #   acc *= (i + 1); // acc = acc * (i + 1)
   # }
   # return acc;
-  cond do
-    i == n -> acc
-    true -> facp1(n, i + 1, acc * (i + 1))
+  defp facp1(n, i, acc) when is_integer(n) do
+    # if i < n do
+    #   facp1(n, i + 1, acc * (i + 1))
+    # else
+    #   acc
+    # end
+    cond do
+      i == n -> acc
+      true -> facp1(n, i + 1, acc * (i + 1))
+    end
+  end
+
+  # fac2(n) = Acc(n, k) * fac2(n - keyword()
+  # Acc(n, k) = n * (n-1) * ... *  (n - k + 1)
+  # Acc(n, k) = Acc(n, k-1) * (n - k + 1)
+  def fac2(n) when is_integer(n) and n >= 0 do
+    cond do
+      n < 1-> 1
+      true -> facp2(n - 1, n)
+    end
+  end
+
+  # defp facp2(n, acc) when is_integer(n) add n == 0 do
+  #   acc
+  # end
+
+  defp facp2(0, acc) when is_integer(acc) do
+    acc
+  end
+
+  defp facp2(n, acc) when is_integer(n) do
+    facp2(n - 1, acc * n)
   end
 end
+
+IO.puts(Factorial.classic(6))
+IO.puts("fac1(0) = #{Factorial.fac1(0)}")
+IO.puts("fac1(1) = #{Factorial.fac1(1)}")
+IO.puts("fac1(2) = #{Factorial.fac1(2)}")
+IO.puts("fac1(3) = #{Factorial.fac1(3)}")
+IO.puts("fac1(4) = #{Factorial.fac1(4)}")
+IO.puts("fac1(5) = #{Factorial.fac1(5)}")
+IO.puts("-----------------------------")
+IO.puts("fac2(0) = #{Factorial.fac2(0)}")
+IO.puts("fac2(1) = #{Factorial.fac2(1)}")
+IO.puts("fac2(2) = #{Factorial.fac2(2)}")
+IO.puts("fac2(3) = #{Factorial.fac2(3)}")
