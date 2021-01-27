ada993c4d0c7131f4e2cf0bbca39a00b30adfed4
Duban Andres <43072051+SrDu001@users.noreply.github.com>
2021-01-27 13:43:15 -0500

    Update keyword_list.ex

--- 01-basics/keyword_list.ex
+++ 01-basics/keyword_list.ex
@@ -1,7 +1,13 @@
 list  = [a: 1, b: 2]
-
+# [{:a, 1},{:b, 2}]
 IO.pits list[:c]  # Returns nil
-#
+# list[index] Throws error
+
+list = [a: 1, a: 1, b: 3, a: 2, b: 2]
+IO.puts list[:a] # 1
+IO.puts list[:b] # 3
+
+IO.puts
 
 # x = []
 # If x = [a: a], then:
