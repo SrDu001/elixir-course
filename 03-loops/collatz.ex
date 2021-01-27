6a199db0d654a52f5c3d29fcb02721a942f3556f
Duban Andres <43072051+SrDu001@users.noreply.github.com>
2021-01-27 17:28:55 -0500

    Collatz exercise

---
+++ 03-loops/collatz.ex
@@ -0,0 +1,30 @@
+defmodule Collatz do
+  def coll(n) when is_integer(n) and n > 0 do
+    cond do
+      rem(n, 2) == 0 -> collp2(n)
+      true -> collp1(n)
+    end
+  end
+
+  defp collp1(n) when is_integer(n) do
+    IO.puts("Impar #{n}")
+    n = (n * 3) + 1
+    cond do
+      rem(n, 2) == 0 -> collp2(n)
+      n == 1 -> IO.puts("Fin #{n}")
+      true -> collp1(n)
+    end
+  end
+
+  defp collp2(n) when is_integer(n) do
+    IO.puts("Par #{n}")
+    n = div(n, 2)
+    cond do
+      rem(n, 2) == 0 -> collp2(n)
+      n == 1 -> IO.puts("Fin #{n}")
+      true -> collp1(n)
+    end
+  end
+end
+
+Collatz.coll(25)
