136e1bad719b434ea1b87b9839af0f282a1ab195
Duban Andres <43072051+SrDu001@users.noreply.github.com>
2021-02-02 16:46:10 -0500

    Update map_reduce.ex
    filter implementation

--- 04-enumerables/map_reduce.ex
+++ 04-enumerables/map_reduce.ex
@@ -10,12 +10,25 @@ defmodule MyMapReduce do
   # defp mapp([], acc, _), do: acc
   # defp mapp([h | t], acc, transform), do: mapp(t, acc ++ [transform.(h)], transform)
 
+  def filter(list, condition) when is_function(condition, 1) do
+    # filterp(list, condition, [])
+    reduce(list, [], fn h, acc -> if.condition?.(h), do: acc ++ [h], else: acc end)
+    reduce(list, [], &(if condition?.(&1), do: &2 ++ [&1], else &2))
+  end
+
+  defp filterp([] , _, result), do: result
+  defp filterp([h | t],condition, result) do
+    newResult = if condition.(h), do: result ++ [h], else: result
+    filterp(t, condition, newResult)
+  end
+
   def reduce(list, acc, action) when is_list(list) and is_function(action, 2) do
     reducep(list, acc,action)
   end
 
   defp reducep([] , acc, _), do: acc
   defp reducep( [h | t], acc, action), do: reducep(t, action.(h, acc), action)
+
 end
 
 # iex.bat .\map_reduce.ex
@@ -29,3 +42,4 @@ end
 # [2, 6, 4]
 IO.inspect MyMapReduce.double([1, 2])
 #IO.puts MyMapReduce.sum([1, 2])
+IO.inspect MyMapReduce.filter([1, 2, 4, 5, 6, 7],&(rem(&1, 2) == 0))
