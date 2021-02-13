a973e96826ce7502bd0906bbde8c41e1ebc3a081
Duban Andres <43072051+SrDu001@users.noreply.github.com>
2021-02-13 14:50:29 -0500

    http-test

---
+++ 12-new-project/http_test/lib/http_test.ex
@@ -0,0 +1,18 @@
+defmodule HttpTest do
+  @moduledoc """
+  Documentation for `HttpTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HttpTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
